38b4b70b83e477f3923d5ce7be187ed480994feb
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-05-05 02:08:32 -0600

    Edit Styling for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,11 +29,11 @@ const Section = tw.div`
 const Container = tw.div`
   h-full
   scroll-snap-align-center
-  w-[90vw]
+  w-[80vw]
   flex
   justify-around
   align-start
-  gap-40
+  gap-50
 `;
 
 const Left = tw.div`
@@ -42,20 +42,22 @@ const Left = tw.div`
   flex-col
   justify-center
   items-start
-  gap-5
-  w-[60%]
+  gap-3
+  w-[40%]
+  h-[80vh]
+
 `;
 
 const NameContainer = tw.h1`
-  flex flex-col w-full h-auto
+  flex flex-col w-full h-auto 
 `;
 
 const Greet = tw.span`
-  text-6xl pb-2
+  text-5xl pb-2
 `;
 
 const Name = tw.strong`
-  text-7xl
+  text-6xl
 `;
 
 const TypedContainer = tw.div`
@@ -108,9 +110,9 @@ export default function HeroBody() {
         <Right>
           <Canvas>
             <Suspense fallback={<Loading/>}>
-              <OrbitControls enableZoom={false} />
+              <OrbitControls enableZoom={false} enableRotate={false}/>
               <ambientLight intensity={1} />
-              <directionalLight position={[3, 2, 1]} />
+              <directionalLight position={[4, 3, 2]} />
               <Sphere args={[1, 100, 200]} scale={2.5}>
                 <MeshDistortMaterial
                   color="#3d1c56"
@@ -121,7 +123,7 @@ export default function HeroBody() {
               </Sphere>
             </Suspense>
           </Canvas>
-          <Image src={headshot} alt='Lex Van-Nugent' priority={true} className="w-[60%] object-contain absolute top-0 bottom-0 left-0 right-0 m-auto animate-bounceSlow rounded-full"/>
+          <Image src={headshot} alt='Lex Van-Nugent' priority={true} className="w-[60%] object-contain absolute top-0 bottom-0 left-0 right-0 m-auto animate-bounceSlow rounded-full hover:shadow-lg hover:shadow-black"/>
         </Right>
         <Left>
          <NameContainer>
@@ -129,27 +131,27 @@ export default function HeroBody() {
             <Name>Lex Van-Nugent</Name>
           </NameContainer>
           <TypedContainer>
-            <span className="text-6xl">A </span>
-            <span className="typer text-secondary text-6xl font-extrabold" ref={el}/>
+            <span className="text-5xl">A </span>
+            <span className="typer text-secondary text-5xl font-extrabold" ref={el}/>
           </TypedContainer>
           <div className="flex flex-row h-auto w-full justify-start space-x-12">
             <div className="w-auto h-auto">
-              <Link href='/contact' className="p-2 bg-secondary/80 rounded-lg shadow-md shadow-black text-xl">Hire Me</Link>
+              <Link href='/contact' className="p-2 bg-secondary/80 rounded-lg shadow-md shadow-black hover:shadow-lg hover:shadow-black text-xl">Hire Me</Link>
             </div>
             <div className="w-auto h-auto">
-              <Link href='/portfolio' className="p-2 bg-secondary/80 rounded-lg shadow-md shadow-black text-xl">View Portfolio</Link>
+              <Link href='/portfolio' className="p-2 bg-secondary/80 rounded-lg shadow-md shadow-black hover:shadow-lg hover:shadow-black text-xl">View Portfolio</Link>
             </div>
           </div>
           <Details>
-            <FaHtml5 className="text-[#e54d26] bg-white rounded-xl" size={50}/>
-            <FaCss3Alt className="text-[#264de4] bg-white rounded-xl" size={50}/>
-            <FaJsSquare className="text-[#e6c72d] bg-white rounded-xl" size={50}/>
-            <FaNodeJs className="text-[#5fa04e] bg-white rounded-xl" size={50}/>
-            <FaReact className="text-[#0fccf7] bg-white rounded-xl" size={50}/>
-            <SiNextdotjs className="text-[#000000] bg-white rounded-xl" size={50}/>
-            <SiPostgresql className="text-[#31648c] bg-white rounded-xl" size={50}/>
-            <SiTailwindcss className="text-[#06adc9] bg-white rounded-xl" size={50}/>
-            <SiThreedotjs className="text-[#000000] bg-white rounded-xl" size={50}/>
+            <FaHtml5 className="text-[#e54d26] bg-white rounded-xl hover:shadow-lg hover:shadow-black" size={50}/>
+            <FaCss3Alt className="text-[#264de4] bg-white rounded-xl hover:shadow-lg hover:shadow-black" size={50}/>
+            <FaJsSquare className="text-[#e6c72d] bg-white rounded-xl hover:shadow-lg hover:shadow-black" size={50}/>
+            <FaNodeJs className="text-[#5fa04e] bg-white rounded-xl hover:shadow-lg hover:shadow-black" size={50}/>
+            <FaReact className="text-[#0fccf7] bg-white rounded-xl hover:shadow-lg hover:shadow-black" size={50}/>
+            <SiNextdotjs className="text-[#000000] bg-white rounded-xl hover:shadow-lg hover:shadow-black" size={50}/>
+            <SiPostgresql className="text-[#31648c] bg-white rounded-xl hover:shadow-lg hover:shadow-black" size={50}/>
+            <SiTailwindcss className="text-[#06adc9] bg-white rounded-xl hover:shadow-lg hover:shadow-black" size={50}/>
+            <SiThreedotjs className="text-[#000000] bg-white rounded-xl hover:shadow-lg hover:shadow-black" size={50}/>
           </Details>
         </Left>
       </Container>
