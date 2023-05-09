c54d9994127309dba64cc5b0bd32f31d9b1e00ce
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-05-08 20:25:09 -0600

    Chnage useref

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRef, useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from "framer-motion"
 import tw from 'tailwind-styled-components';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
@@ -50,8 +50,8 @@ export default function Contact() {
 
   const [map, setMap] = useState(null);
 
-  const contactRef = useRef(null);
-  const leftRef = useRef(null);
+  const contactRef = useRef();
+  const leftRef = useRef();
 
   const contactVariants = {
     hidden: { opacity: 0, y: 50 },
@@ -67,103 +67,105 @@ export default function Contact() {
 
 
   return (
-    <Container id="contact">
-      <LeftSection>
-        <motion.div
-          ref={leftRef}
-          className="w-full h-full flex flex-col items-center justify-center"
-          initial="hidden"
-          whileInView="visible"
-          variants={leftVariants}
-          transition={{ duration: 0.6}}
-        >
-          <div className="my-8 ">
-            <a href="https://github.com/LexVN-WD" target="_blank" rel="noopener noreferrer">GitHub</a>
-            <span className="mx-4">|</span>
-            <a href="https://www.linkedin.com/in/lexvannugent/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          </div>
-          <LoadScript
-              googleMapsApiKey={apiKey}
-            >
-              <GoogleMap
-                id='google-map-script'
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={12}
-                className="w-full h-[80%]"
-              />
-            </LoadScript>
-        </motion.div>
-      </LeftSection>
-      <RightSection>
-        <motion.section
-          ref={contactRef}
-          className="w-full flex flex-col items-center justify-center"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: false, amount: 0.2 }}
-          variants={contactVariants}
-          transition={{ duration: 0.6}}
-        >
-          <h2 className="text-3xl font-bold mb-8">Contact</h2>
-          <form className="w-full max-w-lg">
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3 mb-6 md:mb-0">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="grid-name"
-                >
-                  Name
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-name"
-                  type="text"
-                  placeholder="Name*"
-                />
-              </div>
-              <div className="w-full px-3">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="grid-email"
-                >
-                  Email
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-email"
-                  type="email"
-                  placeholder="Email*"
-                />
-              </div>
+    <AnimatePresence>
+      <Container id="contact">
+        <LeftSection>
+          <motion.div
+            ref={leftRef}
+            className="w-full h-full flex flex-col items-center justify-center"
+            initial="hidden"
+            whileInView="visible"
+            variants={leftVariants}
+            transition={{ duration: 0.6}}
+          >
+            <div className="my-8 ">
+              <a href="https://github.com/LexVN-WD" target="_blank" rel="noopener noreferrer">GitHub</a>
+              <span className="mx-4">|</span>
+              <a href="https://www.linkedin.com/in/lexvannugent/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </div>
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="grid-message"
-                >
-                  Message
-                </label>
-                <textarea
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-message"
-                  placeholder="Enter your message here*"
-                  rows="6"
-                ></textarea>
-              </div>
-            </div>
-            <div className="flex justify-center">
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
+            <LoadScript
+                googleMapsApiKey={apiKey}
               >
-                Send
-              </button>
-            </div>
-          </form>
-        </motion.section>
-      </RightSection>
-    </Container>
+                <GoogleMap
+                  id='google-map-script'
+                  mapContainerStyle={containerStyle}
+                  center={center}
+                  zoom={12}
+                  className="w-full h-[80%]"
+                />
+              </LoadScript>
+          </motion.div>
+        </LeftSection>
+        <RightSection>
+          <motion.section
+            ref={contactRef}
+            className="w-full flex flex-col items-center justify-center"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: false, amount: 0.2 }}
+            variants={contactVariants}
+            transition={{ duration: 0.6}}
+          >
+            <h2 className="text-3xl font-bold mb-8">Contact</h2>
+            <form className="w-full max-w-lg">
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3 mb-6 md:mb-0">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-name"
+                  >
+                    Name
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-name"
+                    type="text"
+                    placeholder="Name*"
+                  />
+                </div>
+                <div className="w-full px-3">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-email"
+                    type="email"
+                    placeholder="Email*"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-message"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-message"
+                    placeholder="Enter your message here*"
+                    rows="6"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                >
+                  Send
+                </button>
+              </div>
+            </form>
+          </motion.section>
+        </RightSection>
+      </Container>
+    </AnimatePresence>
   );
 }
