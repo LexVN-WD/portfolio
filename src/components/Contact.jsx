9ffacc37abe26a6ad3afb0fdb36359e84aa2609b
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-05-09 00:49:14 -0600

    Increase animation distance in contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -59,7 +59,7 @@ export default function Contact() {
   };
 
   const leftVariants = {
-    hidden: { opacity: 0, x: -50 },
+    hidden: { opacity: 0, x: -100 },
     visible: { opacity: 1, x: 0 },
   };
 
